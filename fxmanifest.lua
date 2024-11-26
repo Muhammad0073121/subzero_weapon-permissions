791d5f857db44e228ffc99a4584679a8ac476900
Muhammad Abdullah <mabdullah3121@gmail.com>
2024-11-26 15:54:32 +0500

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+game 'gta5'
+
+Author 'subzer0_07#0000'
+version '1.0.0'
+scriptname 'subzero_weapon-permission-ox'
+
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
+
+lua54 'yes'
+
+escrow_ignore {
+    'config.lua',
+}
+
+dependency 'ox_inventory'
